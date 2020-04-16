ff07cb30043a22177fcdaa2bcb0ae71a82754891
hoony <hoony@hoony.org>
2020-04-16 22:39:37 +0900

    퀴즈4 이벤트 리스너 필요한 경우에만 등록

--- 05/quiz4.js
+++ 05/quiz4.js
@@ -1,6 +1,5 @@
 var $body = document.body;
 var $box = document.querySelector('#box');
-var isDragging = false;
 var pixel = 'px';
 var startX = 0;
 var startY = 0;
@@ -11,29 +10,24 @@ function moveBoxBy(x, y) {
 }
 
 function mouseDownInBox(event) {
-    isDragging = true;
     startX = event.screenX;
     startY = event.screenY;
-}
 
-function mouseMove(event) {
-    if (isDragging) {
-        var deltaX = event.screenX - startX;
-        var deltaY = event.screenY - startY;
-        startX = event.screenX;
-        startY = event.screenY;
-        moveBoxBy(deltaX, deltaY);
-    }
-}
-
-function mouseUpInBox() {
-    isDragging = false;
-}
-
-function addEventListeners() {
-    $box.addEventListener('mousedown', mouseDownInBox);
     $body.addEventListener('mousemove', mouseMove);
     $box.addEventListener('mouseup', mouseUpInBox);
 }
 
-addEventListeners();
+function mouseMove(event) {
+    var deltaX = event.screenX - startX;
+    var deltaY = event.screenY - startY;
+    startX = event.screenX;
+    startY = event.screenY;
+    moveBoxBy(deltaX, deltaY);
+}
+
+function mouseUpInBox() {
+    $body.removeEventListener('mousemove', mouseMove);
+    $box.removeEventListener('mouseup', mouseUpInBox);
+}
+
+$box.addEventListener('mousedown', mouseDownInBox);
