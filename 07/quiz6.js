87ed910ae11ef1cf212daae50402c24962a318ea
hoony <hoony@hoony.org>
2020-04-24 12:55:48 +0900

    [퀴즈6] 구현

---
+++ 07/quiz6.js
@@ -0,0 +1,33 @@
+const $numInput = document.querySelector('#num');
+const $printButton = document.querySelector('#btn');
+const $result = document.querySelector('#result');
+
+function getNumber() {
+    return parseInt($numInput.value);
+}
+
+function makeGugudanString(num) {
+    let result = '';
+    for (let i = 1; i < 10; i++) {
+        result += `${num} x ${i} = ${num * i}<br>`;
+    }
+
+    return result;
+}
+
+function printString(string) {
+    $result.innerHTML = string
+}
+
+function showGugudan() {
+    let num = getNumber();
+
+    if (num) {
+        let result = makeGugudanString(num);
+        printString(result);
+    } else {
+        alert('숫자를 입력해주세요');
+    }
+}
+
+$printButton.addEventListener('click', showGugudan);
