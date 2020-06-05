2cad4ac816b1fd778eea54d852f7ff40bb3af00d
hoony <hoony@hoony.org>
2020-06-05 15:11:21 +0900

    11 수업 과제

---
+++ 11/src/index.js
@@ -0,0 +1,24 @@
+import { render } from './html-render';
+import formInput from './form-input';
+
+import './todos.css';
+
+const $result = document.querySelector('#result');
+
+const todos = [];
+
+$result.addEventListener('click', (event) => {
+  const { className } = event.target;
+  if (className === 'delete') {
+    const { index } = event.target.parentElement.dataset;
+    todos.splice(index, 1);
+    render(todos);
+  } else if (className === 'toggle-checked') {
+    const { index } = event.target.parentElement.dataset;
+    todos[index].isDone = !todos[index].isDone;
+    render(todos);
+  }
+});
+
+formInput.init(todos);
+render(todos);
