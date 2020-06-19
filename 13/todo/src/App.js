5f8a0b73badc095f6f67d530cc29d0212ff70e54
hoony <hoony@hoony.org>
2020-06-19 17:47:45 +0900

    count

--- 13/todo/src/App.js
+++ 13/todo/src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Header from './Header';
 
 function App() {
-  return <div className="App">Hello</div>;
+  const [countNumber, setCountNumber] = useState(0);
+
+  function addCountNumber() {
+    setCountNumber(countNumber + 1);
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div>{countNumber}</div>
+      <button onClick={addCountNumber}>플러스</button>
+    </div>
+  );
 }
 
 export default App;
